70a36292c355e6cd724608461efaf928cb4f7925
Lexî Nuutraî <info@nuutrai.com>
2025-04-02 09:59:05 -0400

    Initial Commit

---
+++ Flagger/FlaggerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlaggerApp.swift
+//  Flagger
+//
+//  Created by Lexi Harrop on 2025-04-02.
+//
+
+import SwiftUI
+
+@main
+struct FlaggerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
